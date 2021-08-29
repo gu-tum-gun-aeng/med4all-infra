996ef8a92fadce7751f7c030e9d0b2e32af2a974
veerapat.b <veerapat.b@ava.fund>
2021-08-29 16:59:08 +0700

    added msk cluster config

--- 07-msk/index.ts
+++ 07-msk/index.ts
@@ -2,12 +2,15 @@ import * as pulumi from '@pulumi/pulumi'
 import Config from './config'
 import Cluster from './cluster'
 import { Network } from '../types/network'
+import clusterConfig from './cluster-config'
 
 const clusterStackRef = new pulumi.StackReference(Config.stackReference)
 const outputNetwork = clusterStackRef.getOutput(
   'createdNetwork'
 ) as pulumi.Output<Network>
 
+export const mskClusterConfig = clusterConfig.create(Config)
+
 export const mskCluster = outputNetwork.apply((network: Network) => {
-  return Cluster.create(Config, network)
+  return Cluster.create(Config, network, mskClusterConfig)
 })
